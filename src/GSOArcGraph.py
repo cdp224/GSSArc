7921539b2170460033d3bd69f5e0d9d295f04bc1
cdp224 <70626472+cdp224@users.noreply.github.com>
2025-06-03 17:10:35 +0200

    created calculation function
    Moved function out of arc calculation loop.

--- src/GSOArcGraph.py
+++ src/GSOArcGraph.py
@@ -3,10 +3,57 @@ import matplotlib.pyplot as plt
 
 
 def angle(v1, v2):
-    # v1 is your first vector
-    # v2 is your second vector
     angle = np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
     return angle
+
+def calc_gso_position(obs_latitude_deg, sat_longitude_deg):
+
+    # GS Satellite Orbit in km
+    Rs = 42164 
+    # Earth radius in km
+    Re = 6371
+
+    obs_latitude_rad = obs_latitude_deg * np.pi / 180
+    sat_longitude_rad = sat_longitude_deg * np.pi / 180
+
+    x_E = 0
+    y_E = Re * np.cos(obs_latitude_rad)
+    z_E = Re * np.sin(obs_latitude_rad)
+    observer = np.array([x_E, y_E, z_E]) #Position of observer
+    print("Observer latitude: " +  str(obs_latitude_deg) + "°")        
+    print("===============================")
+    
+    print("Satellite longitude: " +  str(np.degrees(sat_longitude_rad)) + "°")        
+    #Set position of satellite
+    x_S = Rs * np.sin(sat_longitude_rad)
+    y_S = Rs * np.cos(sat_longitude_rad)
+    z_S = 0
+    satellite = np.array([x_S, y_S, z_S])
+
+    #Calculate point on observers horizontal plane where the line center to satellite penetrates 
+    r=(y_E*y_E+z_E*z_E)/(y_E*y_S)
+    M = np.array(np.multiply(r,[x_S,y_S,0]))
+
+    #calculate elevation (how high is the satellite over the horizon)
+    #Angle between two vectors: OS and OM
+    vec_observer_satellite = np.subtract(satellite, observer)
+    vec_observer_M = np.subtract(M, observer)
+    elevation_rad = angle(vec_observer_M, vec_observer_satellite)
+    elevation_deg = np.degrees(elevation_rad)
+    elevation_sig = np.sign(vec_observer_satellite[1]-vec_observer_M[1])
+    elevation_deg = elevation_sig * elevation_deg
+    print("  Satellite elevation: " + str(elevation_deg) + "°")
+
+    #calculate the azimuth 
+    #Angle between two vectors OMa and OM sat@0 (0 Rs 0)
+    M_aequator = np.array([0,(y_E*y_E+z_E*z_E)/(y_E),0])
+    vec_observer_M_a = np.subtract(M_aequator, observer)
+    azimuth_rad = angle(vec_observer_M_a, vec_observer_M)
+    # azimuth + 180 to accomodate for standard direction to south
+    azimuth_deg = np.sign(sat_longitude_rad) * np.degrees(azimuth_rad) + 180
+    print("  Satellite azimuth:   " + str(azimuth_deg) + "°")
+
+    return azimuth_deg, elevation_deg
 
 def geo_arc(latitude_deg):
     """
@@ -19,63 +66,21 @@ def geo_arc(latitude_deg):
     Returns:
         geo_arc (ndarray): Contains azimuth and elevation in degrees
     """
-    # GS Satellite Orbit in km
-    Rs = 42164 
-    # Earth radius in km
-    Re = 6371
 
     #Observers latitutde # -90 ... 0 ... 90 degrees (south to north)
     #Observers longitude is 0 #Longitude Range: -180 ... 0 ... 180 degrees (West ... 0 ... East)
 
     centerPoint = np.array([0, 0, 0]) # Center point of earth
-    #latitude_deg = 59
-    obs_latitude_rad = latitude_deg * np.pi/180   
-    sat_longitude_rad = np.radians(np.linspace(-90, 90, 100))
 
-    x_E = 0
-    y_E = Re * np.cos(obs_latitude_rad)
-    z_E = Re * np.sin(obs_latitude_rad)
-    observer = np.array([x_E, y_E, z_E]) #Position of observer
-    print("Observer latitude: " +  str(latitude_deg) + "°")        
-    print("===============================")
+    sat_longitude_deg = np.linspace(-90, 90, 100)
 
-    satellite_a = np.array([[0,0,0]])
     geoarc_a = np.array([[0,0]])
 
     #iterate over all given satellite longitudes
-    for longitude_rad in sat_longitude_rad:
+    for longitude_deg in sat_longitude_deg:
 
-        print("Satellite longitude: " +  str(np.degrees(longitude_rad)) + "°")        
-        #Set position of satellite
-        x_S = Rs * np.sin(longitude_rad)
-        y_S = Rs * np.cos(longitude_rad)
-        z_S = 0
-
-        satellite = np.array([x_S, y_S, z_S])
-        satellite_a = np.concatenate((satellite_a, [satellite]), axis=0)
-
-        #Calculate point on observers horizontal plane where the line center to satellite penetrates 
-        r=(y_E*y_E+z_E*z_E)/(y_E*y_S)
-        M = np.array(np.multiply(r,[x_S,y_S,0]))
-
-        #calculate elevation (how high is the satellite over the horizon)
-        #Angle between two vectors: OS and OM
-        vec_observer_satellite = np.subtract(satellite, observer)
-        vec_observer_M = np.subtract(M, observer)
-        elevation_rad = angle(vec_observer_M, vec_observer_satellite)
-        elevation_deg = np.degrees(elevation_rad)
-        elevation_sig = np.sign(vec_observer_satellite[1]-vec_observer_M[1])
-        elevation_deg = elevation_sig * elevation_deg
-        print("  Satellite elevation: " + str(elevation_deg) + "°")
-
-        #calculate the azimuth 
-        #Angle between two vectors OMa and OM sat@0 (0 Rs 0)
-        M_aequator = np.array([0,(y_E*y_E+z_E*z_E)/(y_E),0])
-        vec_observer_M_a = np.subtract(M_aequator, observer)
-        azimuth_rad = angle(vec_observer_M_a, vec_observer_M)
-        # azimuth + 180 to accomodate for standard direction to south
-        azimuth_deg = np.sign(longitude_rad) * np.degrees(azimuth_rad) + 180
-        print("  Satellite azimuth:   " + str(azimuth_deg) + "°")
+        azimuth_deg, elevation_deg = calc_gso_position(latitude_deg, longitude_deg)
+        
         geoarc_point=np.array([azimuth_deg, elevation_deg])
         geoarc_a = np.concatenate((geoarc_a, [geoarc_point]), axis=0)
 
